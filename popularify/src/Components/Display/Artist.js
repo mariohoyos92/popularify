288320e8c6bf9e2bac240d5efdcfe5515fa9bfdd
Mario A. Hoyos <mariohoyos92@gmail.com>
2017-10-23 11:01:39 -0500

    performance enhancements

--- popularify/src/Components/Display/Artist.js
+++ popularify/src/Components/Display/Artist.js
@@ -13,11 +13,9 @@ class Artist extends Component {
         }
     }
 
-
     componentDidMount(){
         axios.get(`http://localhost:3001/api/test/${this.props.selectedGenre}/${this.props.selectedPopularity}`).then((response) => this.setState({recommendations:response.data.tracks.sort( (a,b) => b.popularity - a.popularity)} ))
     }
-
 
     //Everytime that a new genre is selected or the popularity slider is moved, the recommendations are updated below
     componentWillReceiveProps(nextProps){
@@ -25,25 +23,20 @@ class Artist extends Component {
         axios.get(`http://localhost:3001/api/test/${nextProps.selectedGenre}/${nextProps.selectedPopularity}`).then( (response) => {
            return this.setState({recommendations: response.data.tracks.sort( (a,b) => b.popularity - a.popularity)}, () => console.log(this.state.recommendations))});
     }
-
-            
-    shouldComponentUpdate(){
-        return this.state.recommendations
-    }        
+      
     render(){           
         return(
             <div>
             <ReactCSSTransitionGroup
                         transitionName="example"
                         transitionAppear={true}
-      transitionAppearTimeout={1000}
+                        transitionAppearTimeout={1000}
                         transitionEnterTimeout={500}
                         transitionLeaveTimeout={300}>
                 {
 
                  this.state.recommendations ? 
                  
-
                  this.state.recommendations.map((recommendation) => {
         
                  return <div className="artist" key={this.state.recommendations.indexOf(recommendation)}>
@@ -56,12 +49,9 @@ class Artist extends Component {
                                 <div className="popularity"><span className="info">POPULARITY: </span>{recommendation.popularity}</div>
                                 <div className="url"><a href={recommendation.external_urls.spotify} target="_blank">Listen Here</a></div>
                             </div>        
-                        </div>})
-                        
-                 
+                        </div>})                 
                         :
-                        ""
-                 
+                        ""            
                 }
                 </ReactCSSTransitionGroup>
             </div>     
